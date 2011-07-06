31f23aafeb7f2b074c8b71e8f62ce3434e59d19a
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-07-06 14:49:43 +1000

    No longer compile coffeescript on the fly in the browser.

---
+++ graph.js
@@ -0,0 +1,239 @@
+(function() {
+  var $, Seq, active, canvas, ctx, deleteEdge, deleteVertex, dirty, distFromEdge, distFromVertex, dot, down, draw, edges, findEdge, findVertex, handlers, minus, moveVertex, moved, nearestOnEdge, newEdge, newVertex, nextId, plus, position, rubberLine, source, splitEdgeAt, square, times, vertexAt, vertices;
+  $ = jQuery;
+  Seq = pazy.Sequence;
+  vertices = new pazy.IntMap();
+  edges = new pazy.IntMap();
+  canvas = null;
+  ctx = null;
+  source = null;
+  active = null;
+  rubberLine = null;
+  moved = false;
+  down = false;
+  dirty = false;
+  nextId = (function() {
+    var last;
+    last = 0;
+    return function() {
+      return last += 1;
+    };
+  })();
+  square = function(x) {
+    return x * x;
+  };
+  minus = function(_arg, _arg2) {
+    var x1, x2, y1, y2;
+    x1 = _arg[0], y1 = _arg[1];
+    x2 = _arg2[0], y2 = _arg2[1];
+    return [x1 - x2, y1 - y2];
+  };
+  plus = function(_arg, _arg2) {
+    var x1, x2, y1, y2;
+    x1 = _arg[0], y1 = _arg[1];
+    x2 = _arg2[0], y2 = _arg2[1];
+    return [x1 + x2, y1 + y2];
+  };
+  times = function(f, _arg) {
+    var x, y;
+    x = _arg[0], y = _arg[1];
+    return [f * x, f * y];
+  };
+  dot = function(_arg, _arg2) {
+    var x1, x2, y1, y2;
+    x1 = _arg[0], y1 = _arg[1];
+    x2 = _arg2[0], y2 = _arg2[1];
+    return x1 * x2 + y1 * y2;
+  };
+  distFromVertex = function(_arg, _arg2) {
+    var x1, x2, y1, y2;
+    x1 = _arg[0], y1 = _arg[1];
+    x2 = _arg2[0], y2 = _arg2[1];
+    return Math.sqrt(square(x2 - x1) + square(y2 - y1));
+  };
+  nearestOnEdge = function(p, _arg) {
+    var d, f, from, to, v, w;
+    from = _arg[0], to = _arg[1];
+    v = vertices.get(from);
+    w = vertices.get(to);
+    d = minus(w, v);
+    f = dot(d, minus(p, v)) / dot(d, d);
+    if (f < 0) {
+      return v;
+    } else if (f > 1) {
+      return w;
+    } else {
+      return plus(v, times(f, d));
+    }
+  };
+  distFromEdge = function(p, e) {
+    return distFromVertex(p, nearestOnEdge(p, e));
+  };
+  findVertex = function(x, y) {
+    var _ref;
+    return (_ref = Seq.find(vertices, function(_arg) {
+      var id, p;
+      id = _arg[0], p = _arg[1];
+      return distFromVertex([x, y], p) < 10;
+    })) != null ? _ref[0] : void 0;
+  };
+  findEdge = function(x, y) {
+    var _ref;
+    return (_ref = Seq.find(edges, function(_arg) {
+      var e, id;
+      id = _arg[0], e = _arg[1];
+      return distFromEdge([x, y], e) < 10;
+    })) != null ? _ref[0] : void 0;
+  };
+  newVertex = function(x, y) {
+    var id;
+    id = nextId();
+    vertices = vertices.plus([id, [x, y]]);
+    return id;
+  };
+  newEdge = function(from, to) {
+    var id;
+    id = nextId();
+    edges = edges.plus([id, [from, to]]);
+    return id;
+  };
+  deleteVertex = function(pid) {
+    var obsolete;
+    obsolete = Seq.select(edges, function(_arg) {
+      var eid, from, to, _ref;
+      eid = _arg[0], _ref = _arg[1], from = _ref[0], to = _ref[1];
+      return from === pid || to === pid;
+    });
+    edges = edges.minusAll(obsolete != null ? obsolete.map(function(_arg) {
+      var eid, ends;
+      eid = _arg[0], ends = _arg[1];
+      return eid;
+    }) : void 0);
+    return vertices = vertices.minus(pid);
+  };
+  deleteEdge = function(eid) {
+    return edges = edges.minus(eid);
+  };
+  moveVertex = function(pid, x, y) {
+    return vertices = vertices.plus([pid, [x, y]]);
+  };
+  splitEdgeAt = function(x, y) {
+    var eid, from, p, to, _ref;
+    eid = findEdge(x, y);
+    if (eid) {
+      _ref = edges.get(eid), from = _ref[0], to = _ref[1];
+      deleteEdge(eid);
+      p = newVertex.apply(null, nearestOnEdge([x, y], [from, to]));
+      newEdge(from, p);
+      newEdge(p, to);
+      return p;
+    }
+  };
+  vertexAt = function(x, y) {
+    return findVertex(x, y) || splitEdgeAt(x, y) || newVertex(x, y);
+  };
+  draw = function() {
+    var tmp, x, y;
+    if (!dirty) {
+      return;
+    }
+    dirty = false;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (rubberLine) {
+      ctx.beginPath();
+      ctx.moveTo.apply(ctx, rubberLine[0]);
+      ctx.lineTo.apply(ctx, rubberLine[1]);
+      ctx.strokeStyle = 'rgb(0,0,200)';
+      ctx.stroke();
+    }
+    edges.each(function(_arg) {
+      var from, id, to, _ref;
+      id = _arg[0], _ref = _arg[1], from = _ref[0], to = _ref[1];
+      ctx.beginPath();
+      ctx.moveTo.apply(ctx, vertices.get(from));
+      ctx.lineTo.apply(ctx, vertices.get(to));
+      ctx.strokeStyle = id === active ? 'rgb(200,0,0)' : 'black';
+      return ctx.stroke();
+    });
+    vertices.each(function(_arg) {
+      var id, x, y, _ref;
+      id = _arg[0], _ref = _arg[1], x = _ref[0], y = _ref[1];
+      ctx.beginPath();
+      ctx.arc(x, y, 5, 0, Math.PI * 2, true);
+      ctx.strokeStyle = 'black';
+      ctx.stroke();
+      ctx.fillStyle = id === active ? 'rgb(200,0,0)' : 'rgb(0,0,200)';
+      return ctx.fill();
+    });
+    if (tmp) {
+      x = tmp[0], y = tmp[1];
+      ctx.fillStyle = 'rgb(200,200,0';
+      ctx.fillRect(x - 3, y - 3, 6, 6);
+      return tmp = null;
+    }
+  };
+  position = function(e) {
+    return [e.pageX, e.pageY];
+  };
+  handlers = {
+    canvas: {
+      mousemove: function(e) {
+        var x, y, _ref;
+        _ref = position(e), x = _ref[0], y = _ref[1];
+        moved = dirty = true;
+        active = findVertex(x, y) || findEdge(x, y);
+        if (down) {
+          if (rubberLine) {
+            return rubberLine[1] = [x, y];
+          } else if (source) {
+            return moveVertex(source, x, y);
+          }
+        }
+      },
+      mousedown: function(e) {
+        var x, y, _ref;
+        _ref = position(e), x = _ref[0], y = _ref[1];
+        down = dirty = true;
+        if (moved) {
+          rubberLine = null;
+        }
+        source = vertexAt(x, y);
+        return moved = false;
+      },
+      mouseup: function(e) {
+        var pos, target, x, y, _ref;
+        down = false;
+        _ref = position(e), x = _ref[0], y = _ref[1];
+        dirty = true;
+        if ((rubberLine != null) && !moved) {
+          if (source) {
+            deleteVertex(source);
+          }
+          source = rubberLine = null;
+        } else if (rubberLine) {
+          target = vertexAt(x, y);
+          if (source !== target) {
+            newEdge(source, target);
+          }
+          rubberLine = null;
+        } else if (source && !moved) {
+          pos = vertices.get(source);
+          rubberLine = [pos, pos];
+        } else {
+          source = null;
+        }
+        return moved = false;
+      }
+    }
+  };
+  $(document).ready(function() {
+    return $('#canvas').each(function() {
+      if (this.getContext) {
+        canvas = this;
+        ctx = this.getContext('2d');
+        setInterval(draw, 100);
+        return $(this).bind(handlers.canvas);
+      }
+    });
+  });
+}).call(this);
